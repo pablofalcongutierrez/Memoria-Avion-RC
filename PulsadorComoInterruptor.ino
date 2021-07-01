fea7e7c22053b934105874537ceee11e929cd1da
pablofalcongutierrez <67880075+pablofalcongutierrez@users.noreply.github.com>
2021-07-01 22:57:34 +0200

    PulsadorComoInterruptor.ino
    Vamos a utilizar el pulsador del joystick como interruptor. En este caso tiene utilidad de encender y apagar un led, pero para el avión servirá para variar entre un giro de los servos completo o que puedan girar hasta un cierto ángulo y así que el avión pueda realizar giros mas suaves.
    
    Como tenemos dos joysticks, el otro se puede barajar la opción de dejar el motor del avión como automático. Es decir, que no haga falta darle potencia desde el joystick izquierdo.
    
    Otra opción, aunque queda mucho más lejana, es dejarlo como interruptor que activa/desactiva el estabilizador automático. Pero la idea es hacer que vuele primero el avión.

---
+++ PulsadorComoInterruptor.ino
@@ -0,0 +1,28 @@
+//pines del led y pulsador
+int pinLed = 13;
+int pinPulsador = 2;
+
+int inicio = 0;
+int estadoInicial = 1;
+int estadoFinal = 1;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(pinPulsador, INPUT);
+  pinMode(pinLed, OUTPUT);
+}
+
+void loop(){
+
+  estadoInicial = digitalRead(pinPulsador);
+
+  //Los simbolos && se comportan como un por
+  if(estadoInicial && estadoFinal == 0){
+    inicio = 1 - inicio;
+    delay(50);
+  }
+  
+  estadoFinal = estadoInicial;
+
+  digitalWrite(pinLed, inicio);
+}
